07930dd490de5e010b7d55c5f9d46c8f3139354a
rakhatyeskenov <56184712+rakhatyeskenov@users.noreply.github.com>
2021-02-24 03:31:48 +0600

    Add files via upload

---
+++ labs-main/lab4/src/app/products.ts
@@ -0,0 +1,76 @@
+export const products = [
+  {
+  image: '<img src="https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SX679_.jpg',
+  name: 'Acer SB220Q bi 21.5 Inches Full HD (1920 x 1080) IPS Ultra-Thin Zero Frame Monitor (HDMI & VGA Port), Black',
+  description: '21.5 inches Full HD (1920 x 1080) widescreen IPS display --- And Radeon free sync technology. No compatibility for VESA Mount --- Refresh rate: 75 hertz - Using HDMI port',
+  rating: 1
+  },
+  {
+  // image: <img src="assets/img/logo.png">,
+  name: 'AMD Ryzen 5 3600 6-Core, 12-Thread Unlocked Desktop Processor with Wraith Stealth Cooler',
+  description: 'The world`s most advanced processor in the desktop PC gaming segment --- Can deliver ultra-fast 100+ FPS performance in the world`s most popular games --- 6 cores and 12 processing threads bundled with the quiet AMD wraith stealth cooler max temps 95°C',
+  rating: 2
+  },
+  {
+  // image: <img src="assets/img/logo.png">,
+  name: 'Seagate Portable 2TB External Hard Drive Portable HDD – USB 3.0 for PC, Mac, PS4, & Xbox - 1-Year Rescue Service (STGX2000400)',
+  description: 'Easily store and access 2TB to content on the go with the Seagate Portable Drive, a USB external hard drive --- Designed to work with Windows or Mac computers, this external hard drive makes backup a snap just drag and drop --- To get set up, connect the portable hard drive to a computer for automatic recognition no software required',
+  
+  rating: 3
+  },
+  {
+  // image: <img src="assets/img/logo.png">,
+  name: 'Corsair Vengeance RGB PRO 16GB (2x8GB) DDR4 3200MHz C16 LED Desktop Memory - Black (CMW16GX4M2C3200C16)',
+  description: 'Make sure this fits by entering your model number. --- Dynamic multi zone RGB lighting --- Next generation software --- Custom performance PCB',
+  rating: 4
+  },
+  {
+  // image: <img src="assets/img/logo.png">,
+  name: 'Corsair RMX Series, RM850x, 850 Watt, 80+ Gold Certified, Fully Modular Power Supply (CP-9020180-NA)',
+  description: '80 Plus Gold certified for lower power consumption, less noise and cooler temperatures --- For maximum cooling surface area and performance --- Zero RPM fan mode for near silent operation at low to medium loads; Continuous output rated temperature: 50°C',
+  rating: 5
+  },
+  {
+  // image: <img src="assets/img/logo.png">,
+  name: 'Intel Core i7-10700K Desktop Processor 8 Cores up to 5.1 GHz Unlocked  LGA1200 (Intel 400 Series Chipset) 125W (BX8070110700K)',
+  description: '8 Cores / 16 Threads --- Socket type LGA 1200 --- Up to 5.1 GHz unlocked --- Compatible with Intel 400 series chipset based motherboards --- Intel Turbo Boost Max Technology 3.0 support --- Intel Optane Memory support',
+  rating: 6
+  },
+  {
+  // image: <img src="assets/img/logo.png">,
+  name: 'Asus GeForce GTX 1050 Ti 4GB Phoenix Fan Edition DVI-D HDMI DP 1.4 Gaming Graphics Card (PH-GTX1050TI-4G) Graphic Cards',
+  description: 'New nvidia pascal architecture delivers improved performance and power efficiency --- Classic and modern games at 1080p @ 60 fps --- Fast, smooth, power efficient gaming experiences; Memory clock: 7008 mhz',
+  rating: 7
+  },
+  {
+  // image: <img src="assets/img/logo.png">,
+  name: 'ASUS ROG Strix B550-F Gaming (WiFi 6) AMD AM4 Zen 3 Ryzen 5000 & 3rd Gen Ryzen ATX Gaming Motherboard (PCIe 4.0, 2.5Gb LAN, BIOS Flashback, HDMI 2.1, Addressable Gen 2 RGB Header and Aura Sync)',
+  description: 'AMD AM4 Socket and PCIe 4. 0: The perfect pairing for Zen 3 Ryzen 5000 & 3rd Gen AMD Ryzen CPUs --- Robust Power Design: 12+2 DrMOS power stages with high-quality alloy chokes and durable capacitors to provide reliable power for the latest high-core-count AMD CPUs --- Optimized Thermal Solution: Fanless VRM and chipset heatsinks with ASUS Stack Cool 3+ design keep your system running reliably under heavy load by enhancing passive cooling capacity for critical onboard components.',
+  rating: 8
+  },
+  {
+  // image: <img src="assets/img/logo.png">,
+  name: 'ID-COOLING ZOOMFLOW 360X ARGB CPU Water Cooler 5V Addressable RGB AIO Cooler 360mm CPU Liquid Cooler 3X120mm RGB Fan, Intel 115X/1200/2066, AMD AM4',
+  description: '【Better Heat Dissipation】The cpu water cooler equipped with 360mm radiator and 3x120mm PWM fans to make sure excellent heat transfer from cpu. --- 【RGB Lighting】RGB lighting on pump and fans are adjustable to match your PC build. --- PWM Fans】Three 120mm RGB PWM fans deliver a blast of color and improved airflow for extreme CPU cooling performance. Max. Air Flow: 62CFM; Noise Level: 18～26.4dB(A)',
+  rating: 9
+  },
+  {
+  // image: <img src="assets/img/logo.png">,
+  name: 'GOLDEN FIELD Z2 PC Case Gaming Computer Case ATX Cases Mid Tower Tempered Glass Side Panels',
+  description: '【SPEED】Maximum Support 2 x HDD + 3 x SSD --- 【SPACE】Support 400MM/15.75in Graphic Card, Support 160MM/6.3in Length CPU Air Cooler --- 【MOTHERBOARD】Support ATX/M-ATX/ITX Motherboard, compatible with ATX standard power',
+  rating: 10
+  },
+  // {
+  //   id: 3,
+  //   name: 'Phone Standard',
+  //   price: 299,
+  //   description: ''
+  // }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
